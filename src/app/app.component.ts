f3d5db59e84ff91434112f9c6010b671b4ef5397
Sasha <avitchinov@shakuro.com>
2024-06-19 20:02:30 +0300

    feature(group-app): gathering together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { map, take } from 'rxjs';
+import { SlideComponent } from './slide/slide.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [SlideComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'group-app';
+
+  constructor(private readonly httpClient: HttpClient) {}
+
+  private readonly url = '../assets/data.json';
+
+  slides: Record<string, any>[];
+
+  ngOnInit(): void {
+    this.httpClient
+      .get(this.url)
+      .pipe(
+        take(1),
+        map((slide: Record<string, any>) =>
+          Object.keys(slide).map((key) => slide[key])
+        )
+      )
+      .subscribe((slides) => {
+        this.slides = slides;
+      });
+  }
 }
